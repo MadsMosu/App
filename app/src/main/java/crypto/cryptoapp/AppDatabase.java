a888d68a878aa9637fa530cbefede6a2b58fe919
MadsMosu <mads1108@hotmail.dk>
2018-05-04 11:23:04 +0200

    db

---
+++ app/src/main/java/crypto/cryptoapp/AppDatabase.java
@@ -0,0 +1,27 @@
+package crypto.cryptoapp;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+/**
+ * Created by Mads on 04-05-2018.
+ */
+
+@Database(entities = {Asset.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    private static AppDatabase INSTANCE;
+    public abstract AssetDAO assetDao();
+
+    public static AppDatabase getInMemoryDatabase(Context context) {
+        if(INSTANCE == null) {
+            INSTANCE = Room.inMemoryDatabaseBuilder(context.getApplicationContext(), AppDatabase.class).allowMainThreadQueries().build();
+        }
+        return INSTANCE;
+    }
+
+    public static void destroyInstance() {
+        INSTANCE = null;
+    }
+}
